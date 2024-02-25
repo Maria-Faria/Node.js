f77c5bda6d5770d4bc1a9df50c0024184a282646
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-02-24 21:20:18 -0300

    API node com Postgres

---
+++ create-table.js
@@ -0,0 +1,16 @@
+import {sql} from './dbsql.js'
+
+sql`DROP TABLE IF EXISTS videos`.then(() => {
+    console.log('Tabela apagada')
+})
+
+sql `
+CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    duration INTEGER
+);
+`.then(() => {
+    console.log('Tabela criada')
+})
